e278c17c18bcf37e7f0f35cf99e969cbd1682ca9
김민주 <alswn8065@gmail.com>
2023-07-04 11:10:19 +0900

    검색창 클릭 이벤트와 영역 외 클릭 시 숨김 기능 개발

--- src/Browse.js
+++ src/Browse.js
@@ -39,6 +39,8 @@ function Browse() {
     let [specialMovieFunc, setSpecialMovieFunc] = useState();
     let specialMovieInit;
 
+    const searchRef = useRef(null);
+
     useEffect(() => {
         const accessToken = sessionStorage.getItem("accessToken");
         const loginType = sessionStorage.getItem("loginType");
@@ -103,7 +105,20 @@ function Browse() {
         }
         setSpecialMovieFunc(specialMovieInit);
 
+        // 검색창 외 영역 클릭 감지
+        function handleOutside(e) {
+            // current.contains(e.target) : 컴포넌트 특정 영역 외 클릭 감지를 위해 사용
+            if (searchRef.current && !searchRef.current.contains(e.target) && document.querySelector('.searchBox').classList.contains('active')) {
+                document.querySelector('.searchBox').classList.toggle('active');
+            }
+        }
+        document.addEventListener("mousedown", handleOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleOutside);
+        };
+
     }, []);
+
 
     return (
         <>
@@ -130,21 +145,23 @@ function Browse() {
                                             </ul>
                                             <div className="secondary-navigation">
                                                 <div className="nav-element">
-                                                    <div className="searchBox">
-                                                        <button className="searchTab" aria-label="검색">
+                                                    <div className="searchBox" ref={searchRef}>
+                                                        <button className="searchTab" aria-label="검색" onClick={() => {
+                                                            document.querySelector('.searchBox').classList.toggle('active');
+                                                        }}>
                                                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="search-icon" data-name="Search">
                                                                 <path fillRule="evenodd" clipRule="evenodd" d="M14 11C14 14.3137 11.3137 17 8 17C4.68629 17 2 14.3137 2 11C2 7.68629 4.68629 5 8 5C11.3137 5 14 7.68629 14 11ZM14.3623 15.8506C12.9006 17.7649 10.5945 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 12.1076 15.7749 13.1626 15.368 14.1218L24.0022 19.1352L22.9979 20.8648L14.3623 15.8506Z" fill="currentColor"></path>
                                                             </svg>
                                                         </button>
 
-                                                        {/*<div className="searchInput">*/}
-                                                        {/*    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="search-icon ltr-0 e1mhci4z1" data-name="Search">*/}
-                                                        {/*        <path fillRule="evenodd" clipRule="evenodd" d="M14 11C14 14.3137 11.3137 17 8 17C4.68629 17 2 14.3137 2 11C2 7.68629 4.68629 5 8 5C11.3137 5 14 7.68629 14 11ZM14.3623 15.8506C12.9006 17.7649 10.5945 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 12.1076 15.7749 13.1626 15.368 14.1218L24.0022 19.1352L22.9979 20.8648L14.3623 15.8506Z" fill="currentColor"></path>*/}
-                                                        {/*    </svg>*/}
-                                                        {/*    <label htmlFor="searchInput" id="searchInput-label" className="visually-hidden">검색</label>*/}
-                                                        {/*    <input type="text" id="searchInput" name="searchInput" placeholder="제목, 사람, 장르" maxLength="80" className="focus-visible" />*/}
-                                                        {/*    <span className="icon-close empty" />*/}
-                                                        {/*</div>*/}
+                                                        <div className="searchInput">
+                                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="search-icon ltr-0 e1mhci4z1" data-name="Search">
+                                                                <path fillRule="evenodd" clipRule="evenodd" d="M14 11C14 14.3137 11.3137 17 8 17C4.68629 17 2 14.3137 2 11C2 7.68629 4.68629 5 8 5C11.3137 5 14 7.68629 14 11ZM14.3623 15.8506C12.9006 17.7649 10.5945 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 12.1076 15.7749 13.1626 15.368 14.1218L24.0022 19.1352L22.9979 20.8648L14.3623 15.8506Z" fill="currentColor"></path>
+                                                            </svg>
+                                                            <label htmlFor="searchInput" id="searchInput-label" className="visually-hidden">검색</label>
+                                                            <input type="text" id="searchInput" name="searchInput" placeholder="제목, 사람, 장르" maxLength="80" className="focus-visible" />
+                                                            <span className="icon-close empty" />
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div className="nav-element show-kids">
